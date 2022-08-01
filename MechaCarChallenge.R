c9ce95e0d0615f5fe4c7de76e37b1a039ddee709
jenchick <jchickow@gmail.com>
2022-07-31 20:39:39 -0500

    Adding Deliv 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -15,3 +15,20 @@ head(SusCoil)
 total_summary <- SusCoil %>% summarize (Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
 
 lot_summary <- SusCoil %>% group_by(Manufacturing_Lot) %>% summarize (Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+# Deliverable 3: T-Tests on Suspension Coils
+?t.test
+# RScript is written for t-test that compares all manufacturing lots against mean PSI of the population
+lot_sample <- SusCoil %>% sample_n(50) #randomly sample 50 data points
+plt <- ggplot(lot_sample,aes(x=log10(PSI))) #import dataset into ggplot2
+plt + geom_density() #visualize distribution using density plot
+
+t.test(SusCoil$PSI, mu=1500) 
+
+t.test(lot_sample$PSI, mu=1500)
+
+# RScript is written for three t-tests that compare each manufacturing lot against mean PSI of the population
+lot_matrix <- as.matrix(lot_summary) 
+cor(lot_matrix) #Nope
+
+
